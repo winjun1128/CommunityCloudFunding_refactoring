17843c03dd4fc64cce06c1531ecffa6e795080ed
hanjuseung <rhdqn0501@naver.com>
2025-06-03 19:00:20 +0900

    하트클릭하면 위시리스트에 카드 추가 기능

--- src/components/Wishlist.js
+++ src/components/Wishlist.js
@@ -1,6 +1,18 @@
+import { useProducts } from "../data/ProductContext";
+import {Row, Col } from 'react-bootstrap'
+import ProjectCard from "./ProjectCard";
 function Wishlist() {
+    const { products, setProducts } = useProducts();
+    const heartproducts = products.filter(item => item.heart.includes(localStorage.getItem('id')));
     return (
-        <h1>위시리스트 페이지</h1>
+        <div>
+            <h1>위시리스트</h1>
+            <Row xs={1} md={2} lg={3} className="g-4">
+                {heartproducts.map((item) => (
+                    <Col><ProjectCard item={item} /></Col>
+                ))}
+            </Row>
+        </div>
     );
 }
 
